948c99778a46b358d72d1f9778d26d712d08d2f3
Humberto Borba <humberos@gmail.com>
2016-02-07 03:04:48 +0100

    Revert "shinano: Comment out keystore build for now"
    This reverts commit 44ae6671a5a4b1321918d980e1dac8e0fefbb414.
    
    Change-Id: Icc76fa6af5126ce5aea999b484f81e2c3c2f85c6

--- device.mk
+++ device.mk
@@ -160,8 +160,8 @@ PRODUCT_PACKAGES += \
     camera.msm8974
 
 # Keymaster
-#PRODUCT_PACKAGES += \
-#    keystore.msm8974
+PRODUCT_PACKAGES += \
+    keystore.msm8974
 
 # OSS
 PRODUCT_PACKAGES += \
